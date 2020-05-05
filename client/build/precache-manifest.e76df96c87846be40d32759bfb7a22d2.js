5d1340e99198bb5f559dd36f43a718d2c64c0763
dijinio <dijinio@gmail.com>
2020-05-05 17:34:58 +0100

    fin

---
+++ client/build/precache-manifest.e76df96c87846be40d32759bfb7a22d2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb706b397e67418f10f7c99110d20b0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b46fefd963c87b1c2311",
+    "url": "/static/css/main.a494955a.chunk.css"
+  },
+  {
+    "revision": "4b6473e434ceb3a8f5aa",
+    "url": "/static/js/2.8efccb7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8efccb7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b46fefd963c87b1c2311",
+    "url": "/static/js/main.28a0d8e6.chunk.js"
+  },
+  {
+    "revision": "86d69277b111c13af0d4",
+    "url": "/static/js/runtime-main.9739e6da.js"
+  },
+  {
+    "revision": "5e80d46593087328094aff6df3c80adf",
+    "url": "/static/media/clock.5e80d465.png"
+  },
+  {
+    "revision": "a0ae3722b50be63da243a21c5aeeae5b",
+    "url": "/static/media/flag.a0ae3722.png"
+  },
+  {
+    "revision": "95751f94b273296306ca54794a26ced2",
+    "url": "/static/media/minesweeper_logo.95751f94.png"
+  },
+  {
+    "revision": "2d09f7dac5fc73ae5b59d79c47d09dc7",
+    "url": "/static/media/sad.2d09f7da.png"
+  },
+  {
+    "revision": "0cc72ca4ae51505cbc277fdba852ba18",
+    "url": "/static/media/smile.0cc72ca4.png"
+  },
+  {
+    "revision": "39c831077c935e2f0e663601935353de",
+    "url": "/static/media/sunglasses.39c83107.png"
+  }
+]);
